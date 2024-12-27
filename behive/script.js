2d3d6a900d26a52b95a78bb38a629be1fa2264b2
Izzy Beh <israelbeh@gmail.com>
2024-12-27 15:25:41 -0700

    Adding calcTotals function.

--- behive/script.js
+++ behive/script.js
@@ -1,21 +1,52 @@
 class App {
     paycheckAmount = 0;
-    taxDeductions = 0;
+    taxes = 0;
     takeHomePay = 0;
     spendingCategories = {};
     remaining = 0;
 
     calcTakeHomePay() {
-        this.paycheckAmount = this.spendingCategories.gross;
-        // this.taxDeductions = this.spendingCategories.
-        this.takeHomePay = this.paycheckAmount - this.taxDeductions;
-        return this.takeHomePay;
-    }
+        this.paycheckAmount = this.spendingCategories.pay.gross;
+        let taxes = 0;
+        for (const [key, tax] of Object.entries(this.spendingCategories.taxes)) {
+            taxes = tax+taxes;
+        }
 
-    calcTotal(category) {
+        this.taxes = taxes;
+        this.takeHomePay = this.paycheckAmount - taxes;
+    };
+
+    addCategory(category, subCategory, val) {
+        let categoryObj = {};
+
+        categoryObj[subCategory] = val;
+
+        if (category in app.spendingCategories) {
+            Object.assign(app.spendingCategories[category], categoryObj);
+        } else {
+            app.spendingCategories[category] = categoryObj;
+        }
+    };
+
+    calcTotals() {
         // Go through each spending category and sum all subcategories
         // Return the total for the spending category
+        for (let [category, subCategories] of Object.entries(this.spendingCategories)) {
+            let total = 0,
+                totalObj = {};
+            
+            console.log('cat:', category);
+
+            for (let [subCategory, value] of Object.entries(subCategories)) {
+                totalObj['total'] = value + total;
+                console.log('total', total);
+            }
+
+            Object.assign(app.spendingCategories[category], totalObj);
+        }
     };
+
+
 }
 
 const app = new App();
